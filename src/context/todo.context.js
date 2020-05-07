50c1d3dc983a5fa8d2ec07efeb9937a50a9c5eb6
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-05-07 17:03:29 +0300

    Splitting into 2 Contexts

--- src/context/todo.context.js
+++ src/context/todo.context.js
@@ -3,17 +3,20 @@ import React, { createContext, useReducer } from "react";
 import todoReducer from "../reducers/todo.reduces";
 
 export const TodosContext = createContext();
+export const DispatchContext = createContext();
 
 const defaultTodo = [
-  { id: 1, task: "New Task!!!", completed: true },
-  { id: 1, task: "New Task!!!", completed: true },
+  { id: 1, task: "New Task__1!!!", completed: true },
+  { id: 2, task: "New Task__2!!!", completed: false },
 ];
 
 export function TodosProvider(props) {
   const [todos, dispatch] = useReducer(todoReducer, defaultTodo);
   return (
-    <TodosContext.Provider value={{ todos, dispatch }}>
-      {props.children}
+    <TodosContext.Provider value={todos}>
+      <DispatchContext.Provider value={dispatch}>
+        {props.children}
+      </DispatchContext.Provider>
     </TodosContext.Provider>
   );
 }
